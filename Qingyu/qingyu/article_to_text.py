ac62718282676aaa8999c5fdedd4a4ab1b76c25c
Helena Simpson <helenarsimpson@icloud.com>
2026-03-09 15:53:10 +0000

    integrating sentiment results into article database

--- Qingyu/qingyu/article_to_text.py
+++ Qingyu/qingyu/article_to_text.py
@@ -1,22 +1,22 @@
-
+import csv
 import sqlite3
 from typing import List
 from bbc_inclusion_signals import extract_article_text
 
+
 DEFAULT_DB_PATH = "Qingyu/qingyu/bbc_education_inclusion.db"
+DEFAULT_SENTIMENT_RESULTS_PATH = "Sentiment/sentiment_results1.csv"
 
 
 def create_txt_files_from_articles(articles: List[tuple]): ##(excerpt, year, month)
-    id =0
     for article in articles:
         month = article[2]
         year = article[1]
+        id = article[0].replace("?", "()").replace("&", "(())").replace("/", "(_)").replace(":", "(__)") # Use the last part of the URL as an ID
         filename = f"Sentiment/articles/{year}-{month}-{id}.txt"
-        id+=1
         text = extract_article_text(article[0])
         with open(filename, 'w', encoding='utf-8') as f:
             f.write(text)
-
 
 def fetch_links_from_db(db_path: str = DEFAULT_DB_PATH) -> List[tuple]:
     
@@ -43,14 +43,40 @@ def fetch_links_from_db(db_path: str = DEFAULT_DB_PATH) -> List[tuple]:
         conn.close()
     return records
 
-def main():
-    articles = fetch_links_from_db()
-    if not articles:
-        print("No articles found in the database.")
-        sys.exit(1)
+def insert_article_sentiment_into_db(db_path: str = DEFAULT_DB_PATH, sentiment_results_path: str = DEFAULT_SENTIMENT_RESULTS_PATH):
+    conn = sqlite3.connect(db_path)
+    try:
+        cursor = conn.cursor()
+        # cursor.execute("""
+        #     ALTER TABLE articles 
+        #                ADD COLUMN 
+        #                sentiment_score REAL
+        # """)
+        # cursor.execute("""
+        #     ALTER TABLE articles 
+        #                ADD COLUMN
+        #                sentiment_label TEXT
+        # """)
+        # conn.commit()
+       
+        with open(sentiment_results_path, "r", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                url = row["file"].split("-")[-1].replace(".txt", "").replace("(())", "&").replace("()", "?").replace("(_)", "/").replace("(__)", ":") # Reconstruct URL from filename
+                
+                if row["chunk"] != "avg":  # Only insert the average sentiment for each article
+                    continue
+                sentiment_score = float(row["top_score"])
+                sentiment_label = row["top_label"]
+                cursor.execute("""
+                    UPDATE articles
+                    SET sentiment_score = ?, sentiment_label = ?
+                    WHERE url = ?
+                """, (sentiment_score, sentiment_label, url))
 
-    create_txt_files_from_articles(articles)
-    print(f"Created {len(articles)} text files from database records.")
+                
+        conn.commit()
+    finally:
+        conn.close()
 
-if __name__ == "__main__":
-    main()
+   
